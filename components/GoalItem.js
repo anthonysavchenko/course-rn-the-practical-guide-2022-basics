688fbbdf0dd33c4eca357b7884da6f3e269029e5
Anthony Savchenko <anthony.savchenko@gmail.com>
2022-07-29 18:12:53 +1000

    Pressable component

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 function GoalItem(props) {
   return (
-    <View style={styles.goalItem}>
-      <Text style={styles.goalText}>{props.text}</Text>
-    </View>
+    <Pressable onPress={props.onDeleteGoal.bind(this, props.id)}>
+      <View style={styles.goalItem}>
+        <Text style={styles.goalText}>{props.text}</Text>
+      </View>
+    </Pressable>
   );
 }
 
